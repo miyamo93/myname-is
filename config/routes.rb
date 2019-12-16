7fcc14e0e87bac5d8f7d97dddaf03808ec49d8f9
miyamo93 <miyamo0802@gmail.com>
2019-12-16 16:23:07 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
-  resources :messages, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]do
+    resources :messages, only: [:index, :create]
+end
   get  '/tweets'  =>  'tweets#chat'
 end
