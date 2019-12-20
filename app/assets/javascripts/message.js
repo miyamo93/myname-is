b42399c6338ca2328ac4c825779027f1a5671299
miyamo93 <miyamo0802@gmail.com>
2019-12-20 22:55:34 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,8 @@
 $(function(){
   $('#new_message').on('submit', function(){
     e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
       url: 取得したリクエストURL,  //同期通信でいう『パス』
       type: 'POST',  //同期通信でいう『HTTPメソッド』
