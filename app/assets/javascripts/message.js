1a08926d0732416de7110838e0a03529984a1b52
miyamo93 <miyamo0802@gmail.com>
2019-12-22 23:37:03 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,3 +49,12 @@
 //     })
 //   })
 // })
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var todo = $('#chat-input').val();
+    console.log(todo)
+    // var formData = new FormData(this);
+    // var url = $(this).attr('action')
+  })
+})
