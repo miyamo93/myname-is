9621f7f5d20df9aad507baf68140eeaf53f879ac
miyamo93 <miyamo0802@gmail.com>
2019-12-26 22:14:29 +0900

    Update image.js

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -4,6 +4,7 @@ $(document).on('turbolinks:load', function () {
     var reader = new FileReader();
     reader.onload = function (e) {
       $(preview).attr('src', e.target.result);
+      $('.signup-body__image--box').remove()
     }
     reader.readAsDataURL(e.target.files[0]);
   });
