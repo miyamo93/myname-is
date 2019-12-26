8c9e8b2db6c5437bcdfdbf6eaee3391d4e38c681
miyamo93 <miyamo0802@gmail.com>
2019-12-26 20:07:58 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_032907) do
+ActiveRecord::Schema.define(version: 2019_12_26_100148) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_12_16_032907) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
