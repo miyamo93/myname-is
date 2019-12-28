615187c6adfcde75c9ebdbde69fe0a221bd5f84a
miyamo93 <miyamo0802@gmail.com>
2019-12-28 21:36:45 +0900

    user_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_173239) do
+ActiveRecord::Schema.define(version: 2019_12_28_122408) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_173239) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
