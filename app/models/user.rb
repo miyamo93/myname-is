2e7c009e1d6d28f58e9d36141e7327c4e6188476
miyamo93 <miyamo0802@gmail.com>
2020-01-02 23:10:49 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-
+        has_many :comments               #commentsテーブルとのアソシエーション
         has_many :messages
         has_many :tweets
         has_many :group_users
