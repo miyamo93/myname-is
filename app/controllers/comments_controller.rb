ec09401622c243433e8a429340223ede0faa00f9
miyamo93 <miyamo0802@gmail.com>
2020-01-02 23:16:46 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    
+    params.require(:comment).permit(:text, :tweet_id).merge(user_id: current_user.id)
   end
 end
