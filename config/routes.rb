8708d1022f018a5ab92d39b33a062afbf2fb01b1
miyamo93 <miyamo0802@gmail.com>
2020-01-02 23:10:51 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppage#index'
   resources :users, only: [:index, :edit, :update, :show]
-  resources :tweets, only: [:index ,:new ,:create,:show]
+  resources :tweets, only: [:index ,:new ,:create,:show] do
+    resources :comments ,only: [:create]
+  end
   resources :groups, only: [:index, :new, :create, :edit, :update]do
     resources :messages, only: [:index, :create]
 
