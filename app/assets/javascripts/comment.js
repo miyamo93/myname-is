142bc4e861a8f0db11d781fa2224c4cd7c3d3b99
miyamo93 <miyamo0802@gmail.com>
2020-01-04 03:44:02 +0900

    Update comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,15 @@
 $(document).on('turbolinks:load', function(){
+  function buildHTML(comment){
+    var html = `<div class="comment__center--box">
+                  <div class="comment__center--box__name">
+                    ${comment.user_name}
+                  </div>
+                  <div class="comment__center--boxx__text">
+                    ${comment.text}
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     console.log(this)
