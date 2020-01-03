ed16363ef1ee7b2589eeeebf8176ca6fe05eb11e
miyamo93 <miyamo0802@gmail.com>
2020-01-04 02:35:25 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,8 +19,8 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
-    @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
+    @comment = Comment.new
   end
 
   private
