86facd086a62cc9a5bbbbc24d759709b6c35e75d
miyamo93 <miyamo0802@gmail.com>
2020-01-04 13:14:11 +0900

    Update tweets_controller.rb

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -5,6 +5,6 @@ class Api::TweetsController < ApplicationController
     # ajaxで送られてくる最後のメッセージのid番号を変数に代入
     last_message_id = params[:id].to_i
     # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
-    @ = group.messages.includes(:user).where("id > #{last_message_id}")
+    @comments = tweet.comments.includes(:user).where("id > #{last_message_id}")
   end
 end
