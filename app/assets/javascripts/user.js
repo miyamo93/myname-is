fa14d1fcb62f493d47ec1a6ed965ec341825082c
miyamo93 <miyamo0802@gmail.com>
2020-01-06 23:30:06 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,10 +1,15 @@
 $(document).on('turbolinks:load', function(){
   function addUser(user) {
     let html = `
-      <div class="chat-group-user clearfix">
-        <p class="chat-group-user__name">${user.name}</p>
-        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    <div class="chat-group-user clearfix">
+      <div class="chat-group-user__image">
+        <img src= ${ user.image }>
       </div>
+      <div class="chat-group-user__box">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">Add</div>
+      </div>
+    </div>
     `;
     $("#user-search-result").append(html);
   }
@@ -26,7 +31,17 @@ $(document).on('turbolinks:load', function(){
       data: { keyword: input },
       dataType: "json"
       .done(function(users) {
-        console.log("成功です");
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
       })
       .fail(function() {
         console.log("失敗です");
