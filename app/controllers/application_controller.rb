51ffa169bd0f89f09fa65dd395ab88a2ca8f5d62
miyamo93 <miyamo0802@gmail.com>
2020-01-08 03:19:04 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -15,5 +15,11 @@ class ApplicationController < ActionController::Base
   private
     def sign_in_required
         redirect_to new_user_session_url unless user_signed_in?
+    end
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'admin' && password == '1993'
+      end
     end
 end
