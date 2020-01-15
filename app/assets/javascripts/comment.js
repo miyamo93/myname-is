0238d42bab403a99579aa3b04eda6c61d105b5c1
miyamo93 <miyamo0802@gmail.com>
2020-01-15 23:24:14 +0900

    Update comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -29,34 +29,4 @@ $(document).on('turbolinks:load', function(){
       $('#comment-btn').prop('disabled', false);
     })
   })
-  var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_comment_id = $('.comment__center--box:last').data("comment-id");
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/tweets",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_comment_id}
-    })
-    .done(function(comments) {
-      if (comment.length !== 0) {
-        //追加するHTMLの入れ物を作る
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(comments, function(i, comment) {
-          insertHTML += buildHTML(comment)
-        });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.comment__center').append(insertHTML);
-        $('#new_comment')[0].reset();
-        $('#comment-btn').prop("disabled", false);
-      }
-    })
-    .fail(function() {
-      console.log('error')
-    });
-  };
 });
